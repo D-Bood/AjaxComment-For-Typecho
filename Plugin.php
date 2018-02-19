70bd1d2a9b5e3a448ffe4c162d9c1add1eb2fba2
D-Bood <dbood2012@gmail.com>
2018-02-19 20:16:39 +0800

    optimized

--- Plugin.php
+++ Plugin.php
@@ -206,4 +206,34 @@ class AjaxComment_Plugin implements Typecho_Plugin_Interface
         echo '<script type="text/javascript" src="' . $plugin_url . 'js/ajaxcomment.min.js?v=20170426"></script>';
     }
 
+/**
+* 获取评论的锚点链接
+*/
+public function get_comment_at($coid) {
+    $db   = Typecho_Db::get();
+    $prow = $db->fetchRow($db->select('parent')->from('table.comments')
+                                 ->where('coid = ? AND status = ?', $coid, 'approved'));
+    $parent = $prow['parent'];
+    if ($parent != "0") {
+        $arow = $db->fetchRow($db->select('author')->from('table.comments')
+                                     ->where('coid = ? AND status = ?', $parent, 'approved'));
+        $author = $arow['author'];
+        $href   = '<a href="#comment-' . $parent . '">@' . $author . '</a>';
+        echo $href;
+    } else {
+        echo '';
+    }
+}
+/**
+* 输出评论内容
+*/
+public function get_filtered_comment($coid){
+    $db   = Typecho_Db::get();
+    $rs=$db->fetchRow($db->select('text')->from('table.comments')
+                                 ->where('coid = ? AND status = ?', $coid, 'approved'));
+    $content=$rs['text'];
+    echo $content;
+}
+
+
 }
