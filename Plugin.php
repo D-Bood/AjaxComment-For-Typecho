83999eb4bd2cace1b000fbd97ec833f9da88f4c9
D-Bood <dbood2012@gmail.com>
2018-03-11 12:12:52 +0800

    use iconfont

--- Plugin.php
+++ Plugin.php
@@ -187,9 +187,9 @@ class AjaxComment_Plugin implements Typecho_Plugin_Interface
         ?>
         <script type="text/javascript">
             var ajaxcomment_url = '<?php echo $options->index('/action/ajaxcomment?do=post&comment_post_ID=' . $archive->cid);?>';
-            var loading_div = '<div id="AjaxComment_loading" style="display:none"><img src="<?php echo $plugin_url; ?>image/loading.gif"/></div>';
-            var success_div = '<div class="AjaxComment_success"><img src="<?php echo $plugin_url; ?>image/success.png" />&nbsp;提交成功.</div>';
-            var error_div = '<div id="AjaxComment_error" style="display:none"><img src="<?php echo $plugin_url; ?>image/error.png"/><span id="AjaxComment_msg"></span></div>';
+            var loading_div = '<div id="AjaxComment_loading" style="display:none"><i class="fa fa-spinner fa-pulse fa-fw"></i></div>';
+            var success_div = '<div class="AjaxComment_success"><i class="fa fa-check" aria-hidden="true" style="color: #4fd94f"></i>&nbsp;提交成功.</div>';
+            var error_div = '<div id="AjaxComment_error" style="display:none"><i class="fa fa-times" aria-hidden="true" style="color: #d9534f"></i><span id="AjaxComment_msg"></span></div>';
             var id_format = 'comment-{id}';
             var respond_id = 'respond-<?php if($archive->is('post')) echo 'post-' . $archive->cid; if($archive->is('page')) echo 'page-' . $archive->cid;?>';
             var comments_order = '<?php echo $options->commentsOrder; ?>';
